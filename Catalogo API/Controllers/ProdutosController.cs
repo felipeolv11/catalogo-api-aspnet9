33e2eaa1df67ff3d33958bc905e36ae49e2374e0
felipeolv11 <felipeeh1524@gmail.com>
2025-12-24 19:37:04 -0300

    criar produtos controller

---
+++ Catalogo API/Controllers/ProdutosController.cs
@@ -0,0 +1,17 @@
+﻿using Catalogo_API.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalogo_API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class ProdutosController : ControllerBase
+{
+    private readonly CatalogoDbContext _context;
+
+    public ProdutosController(CatalogoDbContext context)
+    {
+        _context = context;
+    }
+}
