d4ee3cc7bd6935a472ca10861cf1e18e3ebd8cd9
felipeolv11 <felipeeh1524@gmail.com>
2025-12-24 19:24:22 -0300

    gerar migração ajuste tabelas usando data annotations

--- Catalogo API/Models/Categoria.cs
+++ Catalogo API/Models/Categoria.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Catalogo_API.Models;
 
+[Table("Categorias")]
 public class Categoria
 {
     public Categoria()
@@ -9,8 +12,16 @@ public class Categoria
         Produtos = new Collection<Produto>();
     }
 
+    [Key]
     public int CategoriaId { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
+
     public ICollection<Produto>? Produtos { get; set; }
 }
