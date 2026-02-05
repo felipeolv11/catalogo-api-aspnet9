0892942328c76d80fb134ce2e0d994559e290124
felipeolv11 <felipeeh1524@gmail.com>
2026-02-05 02:38:55 -0300

    ajustar controllers para uso de dtos

--- Catalogo API/Controllers/CategoriasController.cs
+++ Catalogo API/Controllers/CategoriasController.cs
@@ -1,4 +1,6 @@
-﻿using Catalogo_API.Context;
+﻿using AutoMapper;
+using Catalogo_API.Context;
+using Catalogo_API.DTOs;
 using Catalogo_API.Models;
 using Catalogo_API.Repositories.Interfaces;
 using Microsoft.AspNetCore.Http;
@@ -12,74 +14,86 @@ namespace Catalogo_API.Controllers;
 public class CategoriasController : ControllerBase
 {
     private readonly IUnitOfWork _uow;
+    private readonly IMapper _mapper;
 
-    public CategoriasController(IUnitOfWork uow)
+    public CategoriasController(IUnitOfWork uow, IMapper mapper)
     {
         _uow = uow;
+        _mapper = mapper;
     }
 
     [HttpGet]
-    public ActionResult<IEnumerable<Categoria>> Get()
+    public ActionResult<IEnumerable<CategoriaDTO>> Get()
     {
         var categorias = _uow.CategoriaRepository.GetAll();
-        return Ok(categorias);
+
+        if (categorias is null)
+            return NotFound();
+
+        var categoriasDto = _mapper.Map<IEnumerable<CategoriaDTO>>(categorias);
+
+        return Ok(categoriasDto);
     }
 
     [HttpGet("{id:int}", Name = "ObterCategoria")]
-    public ActionResult<Categoria> Get(int id)
+    public ActionResult<CategoriaDTO> Get(int id)
     {
         var categoria = _uow.CategoriaRepository.Get(c => c.CategoriaId == id);
 
         if (categoria is null)
-        {
             return NotFound();
-        }
 
-        return Ok(categoria);
+        var categoriaDto = _mapper.Map<CategoriaDTO>(categoria);
+
+        return Ok(categoriaDto);
     }
 
     [HttpPost]
-    public ActionResult Post (Categoria categoria)
+    public ActionResult<CategoriaDTO> Post (CategoriaDTO categoriaDto)
     {
-        if (categoria is null)
-        {
+        if (categoriaDto is null)
             return BadRequest();
-        }
+
+        var categoria = _mapper.Map<Categoria>(categoriaDto);
 
         var categoriaCriada = _uow.CategoriaRepository.Create(categoria);
         _uow.Commit();
 
+        var categoriaCriadaDto = _mapper.Map<CategoriaDTO>(categoriaCriada);
+
         return new CreatedAtRouteResult("ObterCategoria",
-            new { id = categoriaCriada.CategoriaId }, categoriaCriada);
+            new { id = categoriaCriadaDto.CategoriaId }, categoriaCriadaDto);
     }
 
     [HttpPut("{id:int}")]
-    public ActionResult Put (int id, Categoria categoria)
+    public ActionResult<CategoriaDTO> Put (int id, CategoriaDTO categoriaDto)
     {
-        if (categoria is null)
-        {
+        if (categoriaDto is null)
             return BadRequest();
-        }
+        
+        var categoria = _mapper.Map<Categoria>(categoriaDto);
 
-        _uow.CategoriaRepository.Update(categoria);
+        var categoriaAtualizada = _uow.CategoriaRepository.Update(categoria);
         _uow.Commit();
 
-        return Ok(categoria);
+        var categoriaAtualizadaDto = _mapper.Map<CategoriaDTO>(categoriaAtualizada);
+
+        return Ok(categoriaAtualizadaDto);
     }
 
     [HttpDelete("{id:int}")]
-    public ActionResult<Categoria> Delete(int id)
+    public ActionResult<CategoriaDTO> Delete(int id)
     {
         var categoria = _uow.CategoriaRepository.Get(c => c.CategoriaId == id);
 
         if (categoria is null)
-        {
             return NotFound();
-        }
 
         var categoriaExcluida = _uow.CategoriaRepository.Delete(categoria);
         _uow.Commit();
 
-        return Ok(categoriaExcluida);
+        var categoriaExcluidaDto = _mapper.Map<CategoriaDTO>(categoriaExcluida);
+
+        return Ok(categoriaExcluidaDto);
     }
 }
--- Catalogo API/Controllers/ProdutosController.cs
+++ Catalogo API/Controllers/ProdutosController.cs
@@ -1,4 +1,6 @@
-﻿using Catalogo_API.Context;
+﻿using AutoMapper;
+using Catalogo_API.Context;
+using Catalogo_API.DTOs;
 using Catalogo_API.Models;
 using Catalogo_API.Repositories.Interfaces;
 using Microsoft.AspNetCore.Http;
@@ -12,93 +14,99 @@ namespace Catalogo_API.Controllers;
 public class ProdutosController : ControllerBase
 {
     private readonly IUnitOfWork _uow;
+    private readonly IMapper _mapper;
 
-    public ProdutosController(IUnitOfWork uow)
+    public ProdutosController(IUnitOfWork uow, IMapper mapper)
     {
         _uow = uow;
+        _mapper = mapper;
     }
 
     [HttpGet]
-    public ActionResult<IEnumerable<Produto>> Get()
+    public ActionResult<IEnumerable<ProdutoDTO>> Get()
     {
         var produtos = _uow.ProdutoRepository.GetAll();
 
         if (produtos is null)
-        {
             return NotFound();
-        }
 
-        return Ok(produtos);
+        var produtosDto = _mapper.Map<IEnumerable<ProdutoDTO>>(produtos);
+
+        return Ok(produtosDto);
     }
 
     [HttpGet("{id:int}", Name = "ObterProduto")]
-    public ActionResult<Produto> Get(int id)
+    public ActionResult<ProdutoDTO> Get(int id)
     {
         var produto = _uow.ProdutoRepository.Get(p => p.ProdutoId == id);
 
         if (produto is null)
-        {
             return NotFound();
-        }
 
-        return Ok(produto);
+        var produtoDto = _mapper.Map<ProdutoDTO>(produto);
+
+        return Ok(produtoDto);
     }
 
     [HttpGet("produtos/{id}")]
-    public ActionResult<IEnumerable<Produto>> GetProdutosCategoria(int id)
+    public ActionResult<IEnumerable<ProdutoDTO>> GetProdutosCategoria(int id)
     {
         var produtos = _uow.ProdutoRepository.GetProdutosPorCategoria(id);
         
         if (produtos is null)
-        {
             return NotFound();
-        }
 
-        return Ok(produtos);
+        var produtosDto = _mapper.Map<IEnumerable<ProdutoDTO>>(produtos);
+
+        return Ok(produtosDto);
     }
 
     [HttpPost]
-    public ActionResult Post (Produto produto)
+    public ActionResult<ProdutoDTO> Post (ProdutoDTO produtoDto)
     {
-        if (produto is null)
-        {
+        if (produtoDto is null)
             return BadRequest();
-        }
+
+        var produto = _mapper.Map<Produto>(produtoDto);
 
         var novoProduto = _uow.ProdutoRepository.Create(produto);
         _uow.Commit();
 
+        var novoProdutoDto = _mapper.Map<ProdutoDTO>(novoProduto);
+
         return new CreatedAtRouteResult("ObterProduto",
-            new { id = novoProduto.ProdutoId }, novoProduto);
+            new { id = novoProdutoDto.ProdutoId }, novoProdutoDto);
     }
 
     [HttpPut("{id:int}")]
-    public ActionResult Put(int id, Produto produto)
+    public ActionResult<ProdutoDTO> Put(int id, ProdutoDTO produtoDto)
     {
-        if (id != produto.ProdutoId)
-        {
+        if (id != produtoDto.ProdutoId)
             return BadRequest();
-        }
+
+        var produto = _mapper.Map<Produto>(produtoDto);
 
         var produtoAtualizado = _uow.ProdutoRepository.Update(produto);
         _uow.Commit();
 
-        return Ok(produto);
+        var produtoAtualizadoDto = _mapper.Map<ProdutoDTO>(produtoAtualizado);
+
+        return Ok(produtoAtualizadoDto);
     }
 
     [HttpDelete("{id:int}")]
-    public ActionResult Delete(int id)
+    public ActionResult<ProdutoDTO> Delete(int id)
     {
         var produto = _uow.ProdutoRepository.Get(p => p.ProdutoId == id);
 
         if (produto is null)
-        {
             return NotFound();
-        }
 
         var produtoDeletado = _uow.ProdutoRepository.Delete(produto);
         _uow.Commit();
 
-        return Ok(produtoDeletado);
+        var produtoDeletadoDto = _mapper.Map<ProdutoDTO>(produtoDeletado);
+
+        return Ok(produtoDeletadoDto);
     }
 }
